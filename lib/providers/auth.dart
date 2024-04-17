6179020de46be538c4ce782c23031013a7e5441e
quilan08 <55520058+quilan08@users.noreply.github.com>
2024-04-17 18:53:02 +0000

    Update auth.dart

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -50,15 +50,9 @@ Future<User?> handleRegister ( {required String? email, required String? passwor
     try{
       final User? user = (await auth.createUserWithEmailAndPassword(email: email!, password: password!)).user;
 
-       user?.updateProfile();
+       user!.updateInfo();
 
-        await _db.collection('users').doc(email).set({
-          "uid": user!.uid,
-          "email": email,
-          "username": username,
-          "roles": {"admin": true, "editor": true},
-          "active": true
-        });
+    
 
       return user;
     } catch(e){
